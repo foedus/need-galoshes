9a01dfb4b023d1ff5746ad847eb6dc10875ca88f
Andrew <andrew.smeall@gmail.com>
2012-10-19 00:39:41 -0400

    Made changes to match the chat example in socket.io git

--- app.js
+++ app.js
@@ -1,16 +1,74 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
+//
+// Setup dependecies
+//
+
+var express = require('express')
+  , http = require('http').createServer(app)
   , io = require('socket.io').listen(server);
+
+//
+//Start the app server
+//
+
+var app = express();
+var server = http.createServer(app);
 
 server.listen(8080);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+
+//
+// Configure basic app settings, using stylus/jade/nib to render
+//
+app.set('title','Galosh-Chat')
+app.configure(function () {
+  app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }));
+  app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname);
+  app.set('view engine', 'jade');
+
+  function compile (str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .use(nib());
+  };
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+//
+//Setup Routing
+//
+
+app.get('/', function (req, res) {
+  res.render('index', { layout: false });
+});
+
+//
+//Launch Socket.io
+//
+
+var sio = io.listen(app)
+  , nicknames = {};
+
+sio.sockets.on('connection', function (socket) {
+  socket.on('user message', function (msg) {
+    socket.broadcast.emit('user message', socket.nickname, msg);
+  });
+
+  socket.on('nickname', function (nick, fn) {
+    if (nicknames[nick]) {
+      fn(true);
+    } else {
+      fn(false);
+      nicknames[nick] = socket.nickname = nick;
+      socket.broadcast.emit('announcement', nick + ' connected');
+      io.sockets.emit('nicknames', nicknames);
+    }
+  });
+
+  socket.on('disconnect', function () {
+    if (!socket.nickname) return;
+
+    delete nicknames[socket.nickname];
+    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+    socket.broadcast.emit('nicknames', nicknames);
   });
 });
